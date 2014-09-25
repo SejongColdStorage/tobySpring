989b0398a843b6e08d7387ae79f7ce20c27af424
Sejong Park <devsejong@gmail.com>
2014-09-25 13:18:15 +0900

    .

---
+++ src/main/java/kr/sadalmelik/DaoFactory.java
@@ -0,0 +1,38 @@
+package kr.sadalmelik;
+
+import kr.sadalmelik.dao.UserDao;
+import org.hsqldb.jdbc.JDBCDriver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class DaoFactory {
+    @Bean
+    public UserDao userDao() {
+        UserDao userDao = new UserDao();
+        userDao.setDataSource(dataSource());
+
+        return userDao;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabase dataSource = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.HSQL)
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .addScript("sql/user.sql")
+                .build();
+
+        return dataSource;
+    }
+
+
+}
